76a7047b6eaf9859425ab1e41928582c64692d2c
pchavan <pchavan@fortinet.com>
2025-01-06 17:54:36 +0530

    bulk operation support added

--- widget/view.controller.js
+++ widget/view.controller.js
@@ -1,6 +1,6 @@
 /* Copyright start
     MIT License
-    Copyright (c) 2024 Fortinet Inc
+    Copyright (c) 2025 Fortinet Inc
 Copyright end */
 'use strict';
 (function () {
@@ -8,15 +8,17 @@ Copyright end */
     .module('cybersponse')
     .controller('heatmap100Ctrl', heatmap100Ctrl);
 
-  heatmap100Ctrl.$inject = ['$scope', 'widgetUtilityService', 'heatmapService', 'config', 'modelMetadatasService', '$state' , '$q'];
+  heatmap100Ctrl.$inject = ['$scope', 'widgetUtilityService', 'heatmapService', 'config', 'modelMetadatasService', '$state' , '$q', , '$rootScope'];
 
-  function heatmap100Ctrl($scope, widgetUtilityService, heatmapService, config, modelMetadatasService, $state, $q) {
+  function heatmap100Ctrl($scope, widgetUtilityService, heatmapService, config, modelMetadatasService, $state, $q, $rootScope) {
     $scope.config = config;
     $scope.processing = true;
     $scope.pageState = $state;
     $scope.multipleFieldsItemsData = [];
     $scope.connectorOperationResponse = {};
     $scope.noData = false;
+    $scope.currentTheme = $rootScope.theme.id;
+
 
     function _handleTranslations() {
       widgetUtilityService.checkTranslationMode($scope.$parent.model.type).then(function () {
@@ -35,23 +37,22 @@ Copyright end */
       var moduleMetaData = modelMetadatasService.getMetadataByModuleType($scope.config.resource);
       //to check if dataSource is present and fetch data from connector action or else from API query
       if(moduleMetaData.dataSource){ 
-        var _promises = [executeConnectorAction(moduleMetaData.dataSource)];
+        var _promises = [getRiskDistributionDetails(moduleMetaData.dataSource)];
         if($scope.multipleFieldsItems && $scope.multipleFieldsItems.length > 0){
-          _promises.push(executeConnectorOperation(moduleMetaData.dataSource));
+          _promises.push(getRecordDetails(moduleMetaData.dataSource));
         }
         $q.all(_promises).then(function(response){
           $scope.processing = false;
           $scope.noData = true;
           if(response[0] && response[0].data && response[0].data.length > 0)
           {
-            setDistributionData(response[0].data);
-            $scope.noData = false;
-          }
-          if(response[1] && response[1].data)
-          {
-            $scope.connectorOperationResponse = response[1].data;
-            setMultipleFieldsData($scope.connectorOperationResponse);
-            $scope.noData = false;
+            setRiskDistributionData(response[0].data);
+            if(response[1] && response[1].data)
+              {
+                $scope.connectorOperationResponse = response[1].data;
+                setMultipleFieldsData($scope.connectorOperationResponse);
+                $scope.noData = false;
+              }
           }
         })
       }
@@ -67,14 +68,14 @@ Copyright end */
       }
     }
 
-    function executeConnectorAction(_moduleMetaData){ 
+    function getRiskDistributionDetails(_moduleMetaData){ 
       let _connectorName = _moduleMetaData.connector;
       let _connectorAction = _moduleMetaData.actions.risk_distribution;
-      let payload = { 'indicator': $scope.indicator || '8.8.8.8' };
+      let payload = { 'indicator': $scope.indicator };
       return heatmapService.executeAction(_connectorName, _connectorAction, payload)
     }
 
-    function executeConnectorOperation(_moduleMetaData){
+    function getRecordDetails(_moduleMetaData){
       let _connectorName = _moduleMetaData.connector;
       let _connectorAction = _moduleMetaData.operation;
       let payload = { 'indicator': $scope.indicator };
@@ -84,14 +85,14 @@ Copyright end */
     function executeAPIQuery(){
       heatmapService.getResourceAggregate($scope.config).then(function (response) {
         if (response && response.data['hydra:member'] && response.data['hydra:member'].length > 0) {
-          setDistributionData(response.data['hydra:member']);
+          setRiskDistributionData(response.data['hydra:member']);
         }
       }, function (error) {
         console.log(error);
       });
     }
 
-    function setDistributionData(_responseData){
+    function setRiskDistributionData(_responseData){
       $scope.distribution = angular.copy(_responseData);
       $scope.distributionSegment = 100 / _responseData.length; //width of each section
       $scope.processing = false;
